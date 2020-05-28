0ed4b7853d81ecf3bfbabe453e5cd120e2b8738c
Rakesh Mothukuri <rockey5520@gmail.com>
2020-05-28 18:45:37 +0100

    Calculating Factorial readme

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,8 +1,12 @@
 package readability;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.print("Hello world!");
+        //stage 1
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine().length() > 100 ?"HARD":"EASY");
+
     }
 }
